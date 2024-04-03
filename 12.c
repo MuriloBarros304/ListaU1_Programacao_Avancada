837f945a3e10520c0f948684466552e6a3c24b6e
Murilo Barros <murilobarros304@gmail.com>
2024-04-02 21:41:07 -0300

    Update 12.c

--- 12.c
+++ 12.c
@@ -11,10 +11,10 @@ int main(void){
 
     // comandos
     aloha[2] = value; // válido
-    scanf("%f", aloha); // inválido, 
+    scanf("%f", &aloha); // válido 
     aloha = "value"; // inválido, aloha é um vetor do tipo float, não pode armazenar um literal
     printf("%f", aloha); // inválido, para funcionar o tipo dos dois argumentos do printf devem ser iguais
-    coisas[4][4] = aloha[3]; // inválido, dimensões diferentes
+    coisas[4][4] = aloha[3]; // válido
     coisas[5] = aloha; // inválido, tipos diferentes
     pf = value; // value é um float, pf é um ponteiro de float, ou seja, tipos diferentes
     pf = aloha; // válido, tipos de variáveis float*
