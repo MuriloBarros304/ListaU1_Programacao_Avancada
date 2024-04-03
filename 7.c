f9abf706e3a9c8765b1e45e5401e7a9201764a50
Murilo Barros <murilobarros304@gmail.com>
2024-04-02 21:22:41 -0300

    update

--- 7.c
+++ 7.c
@@ -10,15 +10,17 @@ int main(void){
         mat[x] = x;
     }
     for (x=0;x<4;x++){
-        printf("x[%d] = %d \n", x, (mat[x]));
+        printf("x[%d] = %d \n", x, (mat[x])); // x[3] = 3
     }
     p = mat + 1;
-    printf("*p = %d\n", *p);
+    printf("*p = %d\n", *p); // *p = 1
+    printf("p = mat + 1 -> %p\n", p); // endereço do segundo elemento do vetor 
+
     p = mat;
-    printf("*p = %d\n", *p);
-    x = (*mat);
-    printf("p = mat + 1 -> %p\n", p); // endereço do segundo elemento do vetor
+    printf("*p = %d\n", *p); // *p = 0
     printf("p = mat -> %p\n", p); // endereço do segundo elemento atualizado
-    printf("x = (*mat) -> %d\n", x); // primeiro elemento do vetor 
+
+    x = (*mat);
+    printf("x = (*mat) -> %d\n", x); // primeiro elemento do vetor //0
 // todas as alternativas são válidas
 }
